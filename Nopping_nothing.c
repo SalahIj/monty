37fc3b2382429702f154dc25a90466d82bed7db2
SalahIj <ijbasalaheddine@gmail.com>
2023-10-19 10:12:18 +0000

    SalahIj

---
+++ Nopping_nothing.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * Nopping_nothing - the function name
+ * @stack: first input
+ * @line_number: second input
+ */
+void Nopping_nothing(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+	(void)stack;
+}
